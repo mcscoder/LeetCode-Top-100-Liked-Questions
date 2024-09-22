2feb95a8cb60a77372e4ba1ac9cafddd8ca6f255
mcs <maicongson69@gmail.com>
2024-09-22 23:36:11 +0700

    add: 440. K-th Smallest in Lexicographical Order

---
+++ Daily-Coding-Challenge/440-k-th-smallest-in-lexicagraphical-order/sol.py
@@ -0,0 +1,32 @@
+# Idea and explanation
+# https://www.youtube.com/watch?v=wRubz1zhVqk
+
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        cur = 1
+        i = 1
+
+        def count(cur):
+            res = 0
+            nei = cur + 1
+            while cur <= n:
+                res += min(nei, n + 1) - cur
+                cur *= 10
+                nei *= 10
+            return res
+
+        while i < k:
+            steps = count(cur)
+            if i + steps <= k:
+                cur += 1
+                i += steps
+            else:
+                cur *= 10
+                i += 1
+        return cur
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.findKthNumber(1361, 400))
